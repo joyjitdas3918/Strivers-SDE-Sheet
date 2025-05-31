fb0196ee57ab66aef5bfd0204aa1c789529fbdf3
joyjitdas3918 <138395405+joyjitdas3918@users.noreply.github.com>
2025-05-31 19:47:11 +0530

    95. Longest Common Prefix

---
+++ 95. Longest Common Prefix/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        String pref = strs[0];
+        int prefLen = pref.length();
+
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+                prefLen--;
+                if (prefLen == 0) {
+                    return "";
+                }
+                pref = pref.substring(0, prefLen);
+            }
+        }
+
+        return pref;        
+    }
+    public static void main(String[] args) {
+        Main obj = new Main();
+        String[] input = {"flower", "flow", "flight"};
+        String result = obj.longestCommonPrefix(input);
+        System.out.println(result); // Output: "fl"
+    }
+}
